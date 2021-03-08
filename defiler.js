8aa2a56a9a108316c1178e54b6ccaaadb89934fb
Hassan Chehouri <hassanchehouri2000@hotmail.com>
2021-03-08 11:27:49 -0500

    test3

--- defiler.js
+++ defiler.js
@@ -1,5 +1,6 @@
 function change_pictures(index){
-   
+
+   // section montage video
     if(index ==0){
         document.getElementById("slidechangek1").src = "./assets/images/1-a.png";	  
         document.getElementById("slidechangek2").src = "./assets/images/1-b.png";
@@ -10,7 +11,8 @@ function change_pictures(index){
         document.getElementById("desc1").innerText = "Night City est un montage vidéo réalisé entièrement sur After Effects, il représente une scène d'introduction à une série entièrement fictive.";
         document.getElementById("desc2").innerText = "Le montage Jour de la Terre est un montage entièrement filmé et monté sur Premiere Pro, le message derrière ce montage est de sensibiliser le monde à ne pas poluer son environnement.";
         document.getElementById("desc3").innerText = "Amélie est un projet dont le but est de recréer une bande d'annonce d'un film avec certains clip de celui-ci.";
-        
+
+     //section design   
     }else if (index ==1){
         document.getElementById("slidechangek1").src = "./assets/images/2-a.jpg";	  
         document.getElementById("slidechangek2").src = "./assets/images/2-b.png";
@@ -19,8 +21,10 @@ function change_pictures(index){
         document.getElementById("titre2").innerText = "Eventbrite";
         document.getElementById("titre3").innerText = "Le Visionnaire";
         document.getElementById("desc1").innerText = "Le Projet de l'art Fictif est un projet dont l'imagination et la créativité sont très mise en valeur. Ce projet à été entièrement construit sur Photoshop.";
-        document.getElementById("desc2").innerText = "Le projet Eventbrite est un projet dont j'ai du refaire une maquette d'un site web qui n'étais pas mis à jour depuis plusieurs mois. Ce projet à été entièrement réalisé sur Pohotshop.";
+        document.getElementById("desc2").innerText = "Le projet Eventbrite est un projet dont j'ai du refaire une maquette d'un site web qui n'étais pas mis à jour depuis plusieurs mois. Ce projet à été entièrement réalisé sur Photshop.";
         document.getElementById("desc3").innerText = "Le Visionnaire est un Logo d'une clinique optométrie entièrement faite sur Illustrator.";
+
+    //section photographie
     } else if (index ==2){
         document.getElementById("slidechangek1").src = "./assets/images/3-a.jpg";	  
         document.getElementById("slidechangek2").src = "./assets/images/3-b.jpg";
